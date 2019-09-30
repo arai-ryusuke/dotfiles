c124f4d48e5ca06ceba669117f18aab74a6b2189
arai-ryusuke <Ryusuke.Arai@jp.ricoh.com>
2019-09-30 17:25:17 +0900

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-DOTPATH=~/.dotfiles
+DOTPATH=~/dotfiles
 
 # git が使えるなら git
-if has "git"; then
+if type "git" > /dev/null 2>&1; then
 	git clone --recursive "$GITHUB_URL" "$DOTPATH"
 
-elif has "curl" || has "wget"; then
+elif type "curl" > /dev/null 2>&1|| type "wget" > /dev/null 2>&1; then
 	tarball="https://github.com/b4b4r07/dotfiles/archive/master.tar.gz"
 
-	if has "curl"; then
+	if type "curl" > /dev/null 2>&1; then
 		curl -L "$tarball"
 
-	elif has "wget"; then
+	elif type "wget" > /dev/null 2>&1; then
 		wget -O - "$tarball"
 
 		fi | tar zxv
@@ -20,12 +20,14 @@ elif has "curl" || has "wget"; then
 		mv -f dotfiles-master "$DOTPATH"
 
 	else
-		die "curl or wget required"
+		echo "curl or wget required"
+		exit 1
 fi
 
-cd ~/.dotfiles
+cd ~/dotfiles
 if [ $? -ne 0 ]; then
-	die "not found: $DOTPATH"
+	echo "not found: $DOTPATH"
+	exit 1
 fi
 
 # 移動できたらリンクを実行する
